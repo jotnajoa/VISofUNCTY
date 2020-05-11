dee26094cfe153974b9afbccf278f6e1215b40a0
soonk <jotnajoa@gmail.com>
2020-05-10 21:45:05 -0400

    kk

--- link/main.js
+++ link/main.js
@@ -98,7 +98,7 @@ Promise.all([
     color2='rgba(212, 209, 51, 0.76)';
     color1b='rgba(242, 118, 51, 0.886)'
     color2b='rgba(212, 209, 51, 0.76)';
-    clearcolor='rgba(189, 189, 189, 0.855)'
+    clearcolor='rgba(189, 189, 189, 0.055)'
     var parser = d3.timeParse("%m/%d/%y")
     
     // Conventional Data processing
@@ -560,7 +560,7 @@ setTimeout(secondstage,500)
                        .attr('cy',function(d, i){
                             return height-yscale(     ( sdata[i].vader +sdata1[i].vader ) *1/2        )
                         })
-                       .style('fill','rgba(255, 255, 255, 0.20)')}
+                       .style('fill','rgba(255, 255, 255, 0.05)')}
                     },3000)
                             
             d3.interval(()=>{
@@ -579,7 +579,7 @@ setTimeout(secondstage,500)
                        .attr('cy',function(d, i){
                             return height-yscale(   (sdata[i].textblob+sdata1[i].textblob)*1/2       )
                         })
-                        .style('fill','rgba(255, 255, 255, 0.20)')}
+                        .style('fill','rgba(255, 255, 255, 0.05)')}
                     },3000)
 
         
